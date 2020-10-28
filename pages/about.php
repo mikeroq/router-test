67668b8eb550fb057db56fa89b2b2249c0f9ffde
pwnstar <55674383+pwncraft@users.noreply.github.com>
2020-10-28 15:48:13 -0500

    Add files via upload

---
+++ pages/about.php
@@ -0,0 +1,12 @@
+<div class="row mt-3 mb-3">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                About Page
+            </div>
+            <div class="card-body">
+                <p>This is the about page.</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ pages/home.php
@@ -0,0 +1,16 @@
+<div class="row mt-3 mb-3">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                Home Page
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <?php
+                echo '<p>PHP Test</p>';
+                ?>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
